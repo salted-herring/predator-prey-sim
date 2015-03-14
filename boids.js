8b5a530f75b3c58927ed2e10dc80cc353feb81f9
Adam Heins <mail@adamheins.com>
2015-03-13 20:59:50 -0700

    Updated boids to have smooter movement.

--- boids.js
+++ boids.js
@@ -10,11 +10,13 @@ var ALIGNMENT_WEIGHT = 1.0;
 var COHESION_WEIGHT = 0.10;
 var PREDATOR_WEIGHT = 2.0;
 
-function Boid(x, y, dx, dy) {
-  this.x = x;
-  this.y = y;
-  this.dx = dx;
-  this.dy = dy;
+// Canvas dimensions, set when the script first gets the canvas.
+var SCREEN_WIDTH;
+var SCREEN_HEIGHT;
+
+function Boid(position, velocity) {
+  this.pos = position;
+  this.vel = velocity;
 }
 
 function Vector(x, y) {
@@ -22,22 +24,35 @@ function Vector(x, y) {
   this.y = y;
 }
 
+Vector.prototype.normalize = function() {
+  len = Math.sqrt(this.x * this.x + this.y * this.y);
+  if (len == 0)
+    return;
+  this.x /= len;
+  this.y /= len;
+}
+
+Vector.prototype.scale = function(scalar) {
+  this.x *= scalar;
+  this.y *= scalar;
+}
+
 function calcCenterOfMass(boids) {
-  var xTotal = 0;
-  var yTotal = 0;
+  var centerOfMass = new Vector(0, 0);
   for (var i = 0; i < boids.length; ++i) {
-    xTotal += boids[i].x;
-    yTotal += boids[i].y;
+    centerOfMass.x += boids[i].pos.x;
+    centerOfMass.y += boids[i].pos.y;
   }
-  return new Vector(xTotal / boids.length, yTotal / boids.length);
+  centerOfMass.scale(1 / boids.length);
+  return centerOfMass;
 }
 
 function calcMeanHeading(boids) {
   var dxTotal = 0;
   var dyTotal = 0;
   for (var i = 0; i < boids.length; ++i) {
-    dxTotal += boids[i].dx;
-    dyTotal += boids[i].dy;
+    dxTotal += boids[i].vel.x;
+    dyTotal += boids[i].vel.y;
   }
   return new Vector(dxTotal, dyTotal);
 }
@@ -54,9 +69,9 @@ function calcSeparations(boids) {
 
       // Calculate such that resulting vector points away from the offending
       // neighbour.
-      var dx = boids[i].x - boids[j].x;
+      var dx = boids[i].pos.x - boids[j].pos.x;
       dx = Math.min(dx, SCREEN_WIDTH - dx);
-      var dy = boids[i].y - boids[j].y;
+      var dy = boids[i].pos.y - boids[j].pos.y;
       dy = Math.min(dy, SCREEN_HEIGHT - dy);
 
       if (dx * dx + dy * dy < MIN_SEP_DIST * MIN_SEP_DIST) {
@@ -67,14 +82,6 @@ function calcSeparations(boids) {
   return dists;
 }
 
-function normalize(vector) {
-  len = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
-  if (len == 0)
-    return;
-  vector.x /= len;
-  vector.y /= len;
-}
-
 function calcNewPositions(boids) {
   var dists = calcSeparations(boids);
   var meanHeading = calcMeanHeading(boids);
@@ -83,16 +90,19 @@ function calcNewPositions(boids) {
   for (var i = 0; i < boids.length; ++i) {
 
     // Calculate the cohesion vector.
-    var cx = centerOfMass.x - boids[i].x;
+    var cx = centerOfMass.x - boids[i].pos.x;
     cx = Math.min(cx, SCREEN_WIDTH - cx);
-    var cy = centerOfMass.y - boids[i].y;
+    var cy = centerOfMass.y - boids[i].pos.y;
     cy = Math.min(cy, SCREEN_HEIGHT - cy);
-    var cohesionVector = new Vector(centerOfMass.x - boids[i].x, centerOfMass.y - boids[i].y);
-    normalize(cohesionVector);
+
+    var cohesionVector = new Vector(cx, cy);
+    cohesionVector.normalize();
+    cohesionVector.scale(COHESION_WEIGHT);
 
     // Calculate the heading vector.
-    var alignmentVector = new Vector(meanHeading.x - boids[i].dx, meanHeading.y - boids[i].dy);
-    normalize(alignmentVector);
+    var alignmentVector = new Vector(meanHeading.x - boids[i].vel.x, meanHeading.y - boids[i].vel.y);
+    alignmentVector.normalize();
+    alignmentVector.scale(ALIGNMENT_WEIGHT);
 
     // Calculate the separation vector. Currently all boids within the
     // separation radius are treated equally: is not a continuous function of
@@ -102,32 +112,39 @@ function calcNewPositions(boids) {
       separationVector.x += dists[i][j].x;
       separationVector.y += dists[i][j].y;
     }
-    normalize(separationVector);
+    separationVector.normalize();
+    separationVector.scale(SEPARATION_WEIGHT);
 
     // Put everything together in one vector.
-    var changeVector = new Vector(COHESION_WEIGHT * cohesionVector.x + ALIGNMENT_WEIGHT * alignmentVector.x + SEPARATION_WEIGHT * separationVector.x, COHESION_WEIGHT * cohesionVector.y + ALIGNMENT_WEIGHT * alignmentVector.y + SEPARATION_WEIGHT * separationVector.y);
-    normalize(changeVector);
+    var changeVector = new Vector(cohesionVector.x + alignmentVector.x + separationVector.x,
+        cohesionVector.y + alignmentVector.y + separationVector.y);
+    cohesionVector.normalize();
 
-    // Update the heading of this boid.
-    boids[i].dx = changeVector.x * SPEED;
-    boids[i].dy = changeVector.y * SPEED;
+    // Update the heading of this boid. The heading is the normalized sum of the
+    // current velocity vector and the new velocity vector, such that the boid's
+    // turning is fairly smooth.
+    boids[i].vel.x += changeVector.x * SPEED;
+    boids[i].vel.y += changeVector.y * SPEED;
+    boids[i].vel.normalize();
+    boids[i].vel.scale(SPEED);
 
     // Bound the boid such that it stays on the screen.
-    boids[i].x = (boids[i].x + boids[i].dx) % SCREEN_WIDTH;
-    boids[i].y = (boids[i].y + boids[i].dy) % SCREEN_HEIGHT;
-    if (boids[i].x < 0)
-      boids[i].x = SCREEN_WIDTH - boids[i].x;
-    if (boids[i].y < 0)
-      boids[i].y = SCREEN_HEIGHT - boids[i].y;
+    boids[i].pos.x = (boids[i].pos.x + boids[i].vel.x) % SCREEN_WIDTH;
+    boids[i].pos.y = (boids[i].pos.y + boids[i].vel.y) % SCREEN_HEIGHT;
+    if (boids[i].pos.x < 0)
+      boids[i].pos.x = SCREEN_WIDTH - boids[i].pos.x;
+    if (boids[i].pos.y < 0)
+      boids[i].pos.y = SCREEN_HEIGHT - boids[i].pos.y;
   }
 }
 
 function drawBoid(ctx, boid) {
-  var angle = Math.atan2(boid.dy, boid.dx);
+  var angle = Math.atan2(boid.vel.y, boid.vel.x);
   ctx.beginPath();
-  ctx.moveTo(boid.x + 6 * Math.cos(angle), boid.y + 6 * Math.sin(angle));
-  ctx.lineTo(boid.x + 4 * Math.cos(angle + 2.094), boid.y + 4 * Math.sin(angle + 2.094));
-  ctx.lineTo(boid.x + 4 * Math.cos(angle + 4.189), boid.y + 4 * Math.sin(angle + 4.189));
+  // I <3 magic numbers.
+  ctx.moveTo(boid.pos.x + 6 * Math.cos(angle), boid.pos.y + 6 * Math.sin(angle));
+  ctx.lineTo(boid.pos.x + 4 * Math.cos(angle + 2.094), boid.pos.y + 4 * Math.sin(angle + 2.094));
+  ctx.lineTo(boid.pos.x + 4 * Math.cos(angle + 4.189), boid.pos.y + 4 * Math.sin(angle + 4.189));
   ctx.fill();
 }
 
@@ -143,8 +160,9 @@ function initBoids(num, width, height) {
     var x = Math.floor(Math.random() * width);
     var y = Math.floor(Math.random() * height);
     var vector = new Vector(Math.random(), Math.random());
-    normalize(vector);
-    boids.push(new Boid(x, y, vector.x * SPEED, vector.y * SPEED));
+    vector.normalize();
+    vector.scale(SPEED);
+    boids.push(new Boid(new Vector(x, y), vector));
   }
   return boids;
 }
